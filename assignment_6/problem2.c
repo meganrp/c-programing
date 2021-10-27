79767f2c171057f09e466a4e08590408d6e98499
Michael Kiss <1michael.kiss@gmail.com>
2021-10-26 20:07:53 -0500

    initial working functionality

---
+++ assignment_6/problem2.c
@@ -0,0 +1,123 @@
+#include "problem2.h"
+
+#define max 20
+#define MAX_LINE_LENGTH 80
+
+int space = 0;
+int top = -1; 
+
+
+void print_with_indent(int indent, char * string)
+{
+    printf("%*s%s", indent, "", string);
+}
+
+
+void push(char* stack, char data[80])
+{
+      if(top == max -1) {
+          printf("Stack is full! \n"); 
+      }
+      else
+      {
+            top = top + 1;
+            strcpy(&stack[top], data);
+
+            print_with_indent(space, &stack[top]);
+            space += 3;
+      }
+}
+
+
+void pop(char* stack, char data[80])
+{
+      if(top == -1)
+            printf("Stack is empty!"); 
+      else
+      {
+            strcpy(data, &stack[top]);
+            top = top - 1;
+            space -= 3;
+      }
+}
+
+
+int main(int argc, char **argv)
+{
+    // char stack[max][80], nm[80];
+    // int top, reply;
+    // top = -1;   // Initialize Stack
+
+    char* stack = (char*) malloc((max + 1) * sizeof(char)); 
+    
+
+    char *path;
+    char line[MAX_LINE_LENGTH] = {0};
+    
+    if (argc < 2) {
+        printf("No file argument was passed. \n");
+        return 1;
+    }
+
+    path = argv[1];
+    
+    /* Open file */
+    FILE *file = fopen(path, "r");
+    
+    if (!file)
+    {
+        printf("Could not read file. \n");
+        return 1;
+    }
+    
+    /* Get each line until there are none left */
+    while (fgets(line, MAX_LINE_LENGTH, file))
+    {   
+        /* Print each line */
+        
+        char *checkFunc;
+        checkFunc = strstr(line, "()");
+        if (checkFunc) {
+            // print_with_indent(space, stack[top]);
+            
+            push(stack, line);
+            // printf("Found a function!\n");
+            
+        }
+
+        char *checkReturn; 
+        checkReturn = strstr(line, "return");
+        if (checkReturn) {
+            // printf("Found a return!\n");
+            pop(stack, line);
+            
+        }
+        
+        /* Add a trailing newline to lines that don't already have one */
+        if (line[strlen(line) - 1] != '\n')
+            printf("\n");
+    }
+    
+    /* Close file */
+    if (fclose(file))
+    {
+        return EXIT_FAILURE;
+        perror(path);
+    }
+
+
+    // reply = push(stack,&top,nm);
+    // if(reply == -1)
+    //         printf("\n Stack is Full.\n");
+    // else
+    //         printf("'%s' is Pushed in Stack.\n\n",nm);
+
+    // reply = pop(stack,  &top,nm);
+    // if(reply == -1)
+    //         printf("\n Stack is Empty \n");
+    // else
+    //         printf("Popped string is : %s\n",nm);
+    //         printf("\n");
+
+      return 0;
+}
